29b90787a15f2392e4468f3b4c3f5faa79c3c51c
Félix Chénier <chenier.felix@uqam.ca>
2022-12-21 13:15:26 -0500

    Changed parser.py to nextwheel_parser.py (name clash with mainstream module)

--- python/websocket_test/read_data_file.py
+++ python/websocket_test/read_data_file.py
@@ -1,56 +1,62 @@
-from parser import parse_superframe, parse_power_frame, parse_encoder_frame, \
-    parse_adc_frame, parse_imu_frame, parse_config_frame, parse
+from nextwheel_parser import (
+    parse_superframe,
+    parse_power_frame,
+    parse_encoder_frame,
+    parse_adc_frame,
+    parse_imu_frame,
+    parse_config_frame,
+    parse,
+)
 import matplotlib.pyplot as plt
 
+filename = "long_avec_wifi_sans_ampli_batterie.dat"
+
 # define and adjust figure
-fig = plt.figure(figsize=(12, 8), facecolor='#DEDEDE')
+fig = plt.figure(figsize=(12, 8), facecolor="#DEDEDE")
 
 adc_plot = plt.subplot(4, 1, 1)  # row, col, index
-adc_plot.set_facecolor('#DEDEDE')
-adc_plot.set_title('ADC')
+adc_plot.set_title("ADC")
 
 imu_plot = plt.subplot(4, 1, 2)  # row, col, index
-imu_plot.set_facecolor('#DEDEDE')
-imu_plot.set_title('IMU')
+imu_plot.set_title("IMU")
 
 power_plot = plt.subplot(4, 1, 3)  # row, col, index
-power_plot.set_facecolor('#DEDEDE')
-power_plot.set_title('POWER')
+power_plot.set_title("POWER")
 
 encoder_plot = plt.subplot(4, 1, 4)  # row, col, index
-encoder_plot.set_facecolor('#DEDEDE')
-encoder_plot.set_title('ENCODER')
+encoder_plot.set_title("ENCODER")
 
 
 if __name__ == "__main__":
-    with open('../log_2022-11-18_10-53-22.dat', 'rb') as f:
+    with open(filename, "rb") as f:
         data = f.read()
-        print('file size : ', len(data))
+        print("file size : ", len(data))
         results = parse(data)
 
-        x_vals = [x[0] for x in results['adc_values']]
-        y_vals = [x[1] for x in results['adc_values']]
-        adc_plot.plot(x_vals, y_vals)
+        x_vals = [x[0] for x in results["adc_values"]]
+        y_vals = [x[1] for x in results["adc_values"]]
+        adc_plot.plot(x_vals, y_vals, "-")
 
         # IMU
         imu_plot.cla()
-        imu_plot.set_title('IMU')
-        x_vals = [x[0] for x in results['imu_values']]
-        y_vals = [x[1] for x in results['imu_values']]
+        imu_plot.set_title("IMU")
+        x_vals = [x[0] for x in results["imu_values"]]
+        y_vals = [x[1] for x in results["imu_values"]]
         imu_plot.plot(x_vals, y_vals)
 
         # POWER
         power_plot.cla()
-        power_plot.set_title('POWER')
-        x_vals = [x[0] for x in results['power_values']]
-        y_vals = [x[1][0:3] for x in results['power_values']]
+        power_plot.set_title("POWER")
+        x_vals = [x[0] for x in results["power_values"]]
+        y_vals = [x[1][0:3] for x in results["power_values"]]
         power_plot.plot(x_vals, y_vals)
 
         # ENCODER
         encoder_plot.cla()
-        encoder_plot.set_title('ENCODER')
-        x_vals = [x[0] for x in results['encoder_values']]
-        y_vals = [x[1] for x in results['encoder_values']]
+        encoder_plot.set_title("ENCODER")
+        x_vals = [x[0] for x in results["encoder_values"]]
+        y_vals = [x[1] for x in results["encoder_values"]]
         encoder_plot.plot(x_vals, y_vals)
 
-        plt.show()
+        # plt.show()
+        plt.tight_layout()
