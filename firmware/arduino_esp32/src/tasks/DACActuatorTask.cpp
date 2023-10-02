f94e420ee38f4fcbd2c3a64084f85c6c0efd5bb3
Dominic Létourneau <doumdi@gmail.com>
2023-10-02 10:10:59 -0400

    Trying audio module, not working

--- firmware/arduino_esp32/src/tasks/DACActuatorTask.cpp
+++ firmware/arduino_esp32/src/tasks/DACActuatorTask.cpp
@@ -37,20 +37,24 @@ void DACActuatorTask::run(void *)
 
     size_t index = 0;
 
-    uint8_t stereo_buffer[4]; //Right, left
+    uint8_t stereo_buffer[4] = {0,0,0,0}; //Right, left
     while (1)
     {
-//        uint16_t sample =  uint16_t(120 * sin(2 * M_PI * frequency * index++ / sampling_rate) + 120);
-        uint16_t sample = 0;
-        //Serial.printf("DACActuatorTask::run sample: i: %i %u \n", i, sample);
+        uint16_t sample =  uint16_t(120.0 * sin(2 * M_PI * (double)frequency * (double)index++ / (double)sampling_rate) + 120.0);
+        //uint16_t sample = 0;
+        //Serial.printf("DACActuatorTask::run sample: i: %i %u \n", index, sample);
 
         //DAC will only use the MSB
+        // Right channel
         stereo_buffer[0] = 0;
-        stereo_buffer[1] = sample & 0xFF;
-        stereo_buffer[2] = 0;
-        stereo_buffer[3] = sample & 0xFF;
+        stereo_buffer[1] = (sample) & 0xFF;
+
+        // Left channel
+        //stereo_buffer[2] = 0;
+        //stereo_buffer[3] = sample & 0xFF;
 
         //Write to DAC (will wait if buffer is full)
+        // Only using right channel
         m_dac.writeFrame(stereo_buffer, sizeof(stereo_buffer) / 2);
     }
 }
