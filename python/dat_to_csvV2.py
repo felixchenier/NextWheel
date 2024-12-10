ef617858f9f3a4aa98144bc6b3313b42a8c7033d
Fleu33 <nicolas.fleury-rousseau@polymtl.ca>
2024-12-10 14:32:10 -0500

    Adjustment of dat_to_csv.py

--- python/dat_to_csvV2.py
+++ python/dat_to_csvV2.py
@@ -14,9 +14,9 @@ import json
 encoder_variation = 0.087890625  # degree by click
 wheel_diameter = 0.6096  # m
 
-path = "C:/Users/Nicolas/Desktop/Test2/"
-dat_filename = "log_2000-12-31_19-00-34.dat"
-csv_filename = "sample_data.csv"
+path = r"C:/Users/Nicolas/Desktop/Test2/"
+dat_filename = r"log_2000-12-31_19-02-12.dat"
+csv_filename = r"sample_data.csv"
 
 
 with open(f"{path}calibration.json", "r") as json_file:
@@ -25,20 +25,24 @@ with open(f"{path}calibration.json", "r") as json_file:
 A = np.array(CALIBRATION["Matrix"])
 b = np.array(CALIBRATION["Offset"])
 
-data = read_dat(dat_filename)
+data = read_dat(f"{path}{dat_filename}")
+time = data["Analog"]["Time"]
+new_time = np.linspace(time[0], time[-1], len(time))
+
 FMs = np.dot(A, data["Analog"]["Force"].T).T - b
 
 wheel_angle_position = data["Encoder"]["Angle"] * encoder_variation
+wheel_angle_position = np.interp(
+    time, data["Encoder"]["Time"], wheel_angle_position
+)
 
 ts = ktk.TimeSeries()
-ts.time = data["Encoder"]["Time"]
-ts.data["WheelPosition"] = wheel_angle_position
-
-ts = ts.resample(round(np.mean(1 / (ts.time[1:] - ts.time[:-1]))))
+ts.time = 1 * new_time
+ts.data["WheelPosition"] = 1 * wheel_angle_position
 
 ts2 = ktk.filters.savgol(ts, window_length=3, poly_order=2, deriv=1)
 
-force_dict = {
+df = {
     "Time Force (s)": data["Analog"]["Time"],
     "Force X (N)": FMs[:, 0],
     "Force Y (N)": FMs[:, 1],
@@ -46,23 +50,23 @@ force_dict = {
     "Moment X (Nm)": FMs[:, 3],
     "Moment Y (Nm)": FMs[:, 4],
     "Moment Z (Nm)": FMs[:, 5],
-}
-
-speed_dict = {
-    "Time Speed (s)": data["Encoder"]["Time"],
     "Orientation (deg)": wheel_angle_position - wheel_angle_position[0],
     "Displacement (m)": np.pi
     / 180
     * wheel_diameter
     / 2
     * (wheel_angle_position - wheel_angle_position[0]),
-    "Angular Speed (deg/s)": np.append(0, ts2.data["WheelPosition"]),
+    "Angular Speed (deg/s)": ts2.data["WheelPosition"],
 }
 
-force_df = pd.DataFrame(force_dict)
-speed_df = pd.DataFrame(speed_dict)
+# speed_dict = {
 
-df = pd.concat([force_df, speed_df], axis=1)
+# }
+
+df = pd.DataFrame(df)
+# speed_df = pd.DataFrame(speed_dict)
+
+# df = pd.concat([force_df, speed_df], axis=1)
 
 
 df.to_csv(f"{path}{csv_filename}", index=False)
